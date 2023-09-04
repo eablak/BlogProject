7438016a9dbd24cad4f17f498f4e8e00f0f529ee
Esra Ablak <esrablk9@gmail.com>
2023-09-04 12:05:24 +0300

    remove

--- BlogProjectEsraOrji/BlogProject.Model/Entities/Abstract/BaseEntity.cs
+++ BlogProjectEsraOrji/BlogProject.Model/Entities/Abstract/BaseEntity.cs
@@ -8,7 +8,7 @@ namespace BlogProject.Model.Entities.Abstract
     public abstract class BaseEntity
     {
 
-        public int Id { get; set; }
+        public int Id { get; set; } 
 
         private DateTime _createDate = DateTime.Now;
 
